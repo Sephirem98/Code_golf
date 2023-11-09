70bdc0bd42c80f218766fa1ec1de3ff5e9872385
Sephirem98 <148339997+Sephirem98@users.noreply.github.com>
2023-11-09 16:00:31 +0100

    Create Christmas_Trees.c
    Draw a size ascending range of Christmas trees using asterisks, ranging from size 3 to size 9, each tree separated by a blank line.
    
    A size 3 tree should look like this, with a single centered asterisk for the trunk:
    
       *
      ***
     *****
        *

---
+++ Christmas_Trees.c
@@ -0,0 +1,1 @@
+int main(){for(int i=1;i<8;i++){for(int j=1;j<=i+2;j++){for(int k=0;k<3-j+i;k++)printf(" ");for(int k=0;k<2*j-1;k++)printf("*");printf("\n");}for(int j=0;j<i+2;j++)printf(" ");printf("*\n\n");}}
