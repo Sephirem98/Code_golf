8294b0da0cc759ee2d85b24587d872f24e6baf8c
Sephirem98 <148339997+Sephirem98@users.noreply.github.com>
2023-11-09 16:05:37 +0100

    Create Diamonds.c
    Draw a size ascending range of diamonds using the numbers 1 to 9, ranging from size 1 to size 9, each diamond separated by a blank line.
    
    A size 1 diamond should look like this, a single centered 1:
    
             1
    With the largest size 9 diamond looking like this:
    
                   1
                 121
               12321
             1234321
           123454321
         12345654321
        1234567654321
      123456787654321
     12345678987654321
       123456787654321
         1234567654321
           12345654321
             123454321
               1234321
                 12321
                  121
                    1

---
+++ Diamonds.c
@@ -0,0 +1,1 @@
+int main(){for(int i=1,a=1,b=1;i<=9;i++,a=1){for(int j=1;j!=0;j>=i?a=-1:0,j+=a,b=1){for(int l=0;l<10-j;l++)printf(" ");for(int k=1;k!=0;k>=j?b=-1:0,k+=b)printf("%i",k);printf("\n");}printf("\n");}}
