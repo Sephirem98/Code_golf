4245823655b3ecbeaff9114b228f948c4681fa83
Sephirem98 <148339997+Sephirem98@users.noreply.github.com>
2023-11-10 11:24:04 +0100

    Create FooFizzBuzzBar.c
    Print the numbers from 1 to 1,000 inclusive, each on their own line.
    
    If, however, the number is a multiple of two then print Foo instead, if the number is a multiple of three then print Fizz, if the number is a multiple of five then print Buzz, and if the number is a multiple of seven then print Bar.
    
    If multiple conditions hold true then all replacements should be printed, for example 15 should print FizzBuzz.

---
+++ FooFizzBuzzBar.c
@@ -0,0 +1,1 @@
+int main(){for(int i=1;i<1001;i++){i%2==0||i%3==0||i%5==0||i%7==0?(i%2==0?printf("Foo"):0,i%3==0?printf("Fizz"):0,i%5==0?printf("Buzz"):0,i%7==0?printf("Bar"):0):printf("%i",i);printf("\n");}}
